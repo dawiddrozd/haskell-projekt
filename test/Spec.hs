c929ca9ecbbd48e5e7365d1837d270b8e6338f2c
kacperstopa <kstopa225@gmail.com>
2018-01-25 21:41:28 +0100

    add dialogues, tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,29 @@
+module Main where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import RandomPrimes
+import Rsa
+import Encryption
+import Control.Monad.Fix
+import Vigenere
+import Text.Read
+import System.Directory
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain (testGroup "Our Library Tests" [test1, test2])
+
+test1 = testCase "Vigenere test" (assertEqual "Should return \"test\" " "test" $ vigenereDecrypt (vigenereEncrypt "test" "dupa") "dupa")
+
+rsaTest = do
+    primes <- rndPrimes 10
+    keys <- uncurry publicAndPrivateKey primes
+    let public = fst keys
+    let private = snd keys
+    let coded = rsaEncryptString public "test"
+    let decoded = rsaEncryptString private coded
+    putStrLn decoded
+
+    
+test2 = testCase "RSA test" (assertEqual "Should return " (putStrLn "test") rsaTest)
