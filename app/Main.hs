adab81fe58d12831ab90e44b324c949646b89c5f
brwxq <ddrozdu@gmail.com>
2018-01-25 02:40:52 +0100

    Add file operations and simple version of app

--- app/Main.hs
+++ app/Main.hs
@@ -6,9 +6,24 @@ import Encryption
 import Control.Monad.Fix
 import Vigenere
 import Text.Read
+import System.Directory
 
 main :: IO ()
-main = dialog
+main = do
+    putStrLn "What do you want to do?"
+    putStrLn "[1] Encrypt the message."
+    putStrLn "[2] Decrypt the message from the file."
+    putStr "Your choice: "
+    line <- getLine
+    let maybeNum = readMaybe line :: Maybe Integer
+    case maybeNum of 
+        Just 1 -> dialog
+        Just 2 -> do
+            putStr "Enter the filename: " >> getLine >>= 
+                \fileName -> case fileName of
+                    "rsa" -> decryptRsaFromFile fileName
+                    "vig" -> decryptVigenerFromFile fileName
+        _ -> putStrLn "Incorrect choice. Try again" >> main
 
 dialog :: IO ()
 dialog = putStrLn "Choose encryption algorithm: "
@@ -22,10 +37,10 @@ algorithmChooser = fix $ \repeat -> do
     line <- getLine
     let maybeNum = readMaybe line :: Maybe Integer
     case maybeNum of 
-        Just 1 -> putStrLn "Starting RSA enryption..." >> rsaEncryption
-        Just 2 -> putStrLn "Starting Vigenere encryption..." >> vigenereCipher
+        Just 1 -> rsaEncryption
+        Just 2 -> vigenereCipher
         _ -> do
-            putStrLn "You should choose 1 or 2"
+            putStrLn "Incorrect choice. You should choose 1 or 2."
             repeat
 
 rsaEncryption :: IO ()
@@ -38,11 +53,54 @@ rsaEncryption = do
     keys <- publicAndPrivateKey p q
     let public = fst keys
     let private = snd keys
+    putStrLn "Starting RSA enryption..."
     let coded = encryptString public msg
-    print $ encryptString private coded
+    encryptRsaToFile private coded
 
 vigenereCipher :: IO ()
 vigenereCipher = do
     putStr "What do you want to encrypt? "
     msg <- getLine
-    putStrLn "Kacper tu masz zaszyfrować"
+    let key = "kacper"
+    putStrLn "Starting Vigenere encryption..."
+    let coded = vigenereEncrypt msg key
+    encryptVigenereToFile key coded
+
+encryptRsaToFile :: (Integer,Integer) -> String -> IO ()
+encryptRsaToFile key encryptedMsg = do
+    let fileName = "rsa"
+    writeFile fileName $ show key
+    appendFile fileName $ "\n" ++ encryptedMsg
+    putStrLn $ "Message encrypted using RSA algorithm in file: " ++ fileName
+
+encryptVigenereToFile :: String -> String -> IO ()
+encryptVigenereToFile key encryptedMsg = do
+    let fileName = "vig"
+    writeFile fileName key
+    appendFile fileName $ "\n" ++ encryptedMsg
+    putStrLn $ "Message encrypted in file using Vigenere algorithm in file: " ++ fileName
+
+decryptRsaFromFile :: String -> IO ()
+decryptRsaFromFile fileName = do
+    fileExist <- doesFileExist fileName
+    if not fileExist then putStrLn $ "File " ++ fileName ++ " does not exists!"
+    else do
+        content <- readFile fileName
+        let linesOfFile = lines content
+        let private = read $ linesOfFile !! 0
+        let coded = linesOfFile !! 1
+        putStr "Encrypted message: "
+        print $ encryptString private coded
+
+decryptVigenerFromFile :: String -> IO ()
+decryptVigenerFromFile fileName = do
+    fileExist <- doesFileExist fileName
+    if not fileExist then putStrLn $ "File " ++ fileName ++ " does not exists!"
+        else do
+            content <- readFile fileName
+            let linesOfFile = lines content
+            let key = linesOfFile !! 0
+            let coded = linesOfFile !! 1
+            putStr "Encrypted message: "
+            print $ vigenereDecrypt coded key
+            putStrLn "No i sie zjebalo cos "
